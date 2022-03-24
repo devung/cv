b3480f0afee0120ec5affb469822ac3dd2f9f476
Lim Ung <limung.dev@gmail.com>
2022-03-24 23:46:57 +1100

    refactor

--- index.js
+++ index.js
@@ -1,331 +1,21 @@
+// Global variables
 const _INDENT_SIZE = 4
-const _CHAR_SIZE = 1 // in CH
-let _CV // replace
-let _Cv
-let _CVV // remove
-let _CMDS = [] // replace
-let _CommandList = []
-let _CMD_IDX = null // replace
+const _CHAR_SIZE = 8.8 // in px --> change to CH
+let _cv
+let _commandList = []
+let _cmd_idx = null
+let _userPath = 'C:\\Users\\limung.dev>'
+let _res
 
-let userPath = 'C:\\Users\\limung.dev>' // replace
-let _UserPath = 'C:\\Users\\limung.dev>'
+// Initialize
+loadCV("cv.json")
+loadDefaultConsole(_userPath)
 
-loadJson("cv.json")
-
-loadJsonFile("cv_skillset.json").then( res => {
-    _CVV = res
-})
-
-
-loadDefaultConsole(userPath)
-
-async function loadJson(filePath) {
-    var res = await fetch(filePath)
-    _CV = await res.json()
-}
-
-async function loadJsonFile(filePath) {
-    let js = await fetch(filePath)
-    let res = await js.json()
-    return res
-}
-
-function loadDefaultConsole(initString) {
+// Console
+function loadDefaultConsole(path) {
     let a = document.getElementById('console-log-default')
     a.innerHTML = "Microsoft Windows [Version 10.0.19042.1415]<br>(c) Microsoft Corporation. All rights reserved."
-
-    createInput(initString)
-}
-
-function addToCommandList(command) {
-    const last = _CMDS.at(-1)
-    console.log({last})
-    if (last != command) {
-        _CMDS.push(command)
-        _CMD_IDX = _CMDS.length
-    }
-}
-
-async function invokeCommand(cmd, path) {
-    let err = `'${cmd}' is not recognized as an internal or external command,<br>operable program or batch file.`
-    cmd = cmd.toLowerCase()
-
-    if (cmd) {
-        let res = getJson(cmd)
-        if (res) {
-            err = null
-            printConsoleResult(res)
-        }
-    }
-
-    if (cmd == 'cv') {
-        err = null
-        printConsoleResult(`<div class="cv">${prettyPrint(_CV)}</div>`)
-    }
-    if (cmd == 'cvv') {
-        err = null
-        printConsoleResult(`<div class="cv">${prettyPrint(_CVV)}</div>`)
-    }
-
-    if (cmd == 'help') {
-        err = null
-        let results = await loadFile('help.txt')
-        printConsoleResult(results)
-    }
-    
-    if (cmd == 'cls') {
-        err = ''
-        let c = document.getElementById('console')
-        removeAllChildNodes(c)
-    }
-    
-    if (err) printConsoleResult(err, 'error')
-    if (!err) addToCommandList(cmd)
-    createInput(userPath)
-    toggleHelp(true)
-}
-
-function getJson(key) {
-    let res = _CV[key]
-    if (!res) return null
-
-    let pretty = prettyPrintChild(res)
-
-    return `${pretty}`
-}
-
-function prettyPrintChild(value, key = null, parent = null, pad = 0) {
-    const cn = value.constructor.name
-    console.log('prettyPrintChild - start key: ' + key + ', type of: ' + cn)
-    console.log({ key, value, pad, parent })
-    let pretty
-    if (cn === 'String' || cn === 'Number') {
-        value = value.toString()
-        let r = prettyPrintString(value, key, parent, pad)
-        pretty = (key) ? `<div class="key-text-pair">${r}</div>` : r
-
-    }
-    if (cn === 'Array') {
-        let na = []
-        const ldx = value.length - 1
-        value.forEach((v, i, s) => {
-            console.log({ i, s })
-            const vcn = v.constructor.name
-            let e
-            if (vcn === 'String' || 'Number') {
-                let vStr = v.toString()
-                console.log({key})
-                e = `<div class="element element-text">${prettyPrintChild(vStr, null, null, 0)}</div>`
-            }
-            if (vcn === 'Object') {
-                e = `<div class="element element-object">${prettyPrintChild(v, null, null, 0)}</div>`
-            }
-            if (vcn === 'Array') {e = ''} // TO-DO
-            na.push(e)
-        })
-        let v = `<div class="array">${na.join('')}</div>`
-        pretty = (key && !(parent === key)) ? `<div class="key child">${capitalize(key)}</div>${v}` : v
-    }
-    if (cn === 'Object') {
-        let na = []
-        const kl = Object.keys(value).map(k => k.length)
-        const mx = Math.max(...kl)
-        for (const [k, v] of Object.entries(value)) {
-            console.log({ kl, mx })
-            let p = prettyPrintChild(v, k, key, mx)
-            na.push(p)
-        }
-        let r = na.join('')
-        pretty = `<div class="object">${r}</div>`
-    }
-    console.log('prettyPrintChild - end')
-    return pretty
-}
-
-function prettyPrint(value, key = null, pad = 0) {
-    console.log("prettyPrint - start")
-    console.log({ key, value })
-    const cn = value.constructor.name
-    let pretty
-    if (cn === 'Object') {
-        let na = []
-        for (const [k, v] of Object.entries(value)) {
-            console.log({ k, v })
-            let pkey = k.toUpperCase().replaceAll('_', ' ')
-            let p =  `<div class="Parent-Object"><div class="key parent">${pkey}</div>${prettyPrintChild(v, k, k, 0)}</div>`
-            na.push(p)
-        }
-        pretty = `<div class="Object">${na.join('')}</div>`
-    }
-    console.log(`prettyPrint: end`)
-
-    return pretty
-}
-
-function prettyPrintString(value, key = null, parent = null, pad = 0) {
-    console.log('prettyPrintString - start')
-    console.log(value, key, parent, pad)
-    let res
-    if (key) {
-        let k = capitalize(key).padEnd(pad, ' ')
-        let sv =  `<span class="value">${value}</span>`
-        res = (parent === key) 
-            ? `<span class="key hide" pad="${pad}">${k}</span>${sv}` 
-            : `<span class="key" pad="${pad}">${k}</span>${sv}`
-    } else {
-        res = `<span class="value">${value}</span>`
-    }
-    console.log(`prettyPrintString: end`)
-    return res
-}
-
-function capitalize(string) {
-    return string[0].toUpperCase() + string.slice(1).toLowerCase().replaceAll('_', ' ')
-}
-
-async function loadFile(fileName) {
-    let res = await fetch(fileName)
-    let help = await res.text()
-    return help
-}
-
-function getCommandList(key) {
-    const max = _CMDS.length
-    let i = document.getElementById('console-input-current')
-    const v = i.value
-    console.log({v})
-    if (_CMD_IDX === null) _CMD_IDX = max
-    
-    if (key === 'ArrowUp' && _CMD_IDX != 0) {
-        _CMD_IDX = _CMD_IDX - 1
-    } 
-    if (key === 'ArrowDown' && _CMD_IDX < (max - 1)) {
-        _CMD_IDX = _CMD_IDX + 1
-    }
-    const c = _CMDS[_CMD_IDX]
-    const le = (c) ? c.length : null
-    console.log({_CMD_IDX})
-    console.log({c})
-
-    if (c && (c != v)) {
-        const ts = 8.8
-        const wi = le * ts
-        i.setAttribute('style', `width: ${wi}px`)
-        i.value = c
-    } else {
-        i.focus()
-        i.setSelectionRange(le, le * 10)
-    }
-
-}
-
-function printConsoleResult(results, type = 'success') {
-    // TO-DO - type each character
-    // Create result span tag
-    let c = document.getElementById('console')
-    let cr = document.createElement('div')
-    cr.setAttribute('class', `console-result console-result-${type}`)
-    cr.innerHTML = results
-    c.appendChild(cr)
-
-    c.appendChild(document.createElement('br'))
-}
-
-function toggleHelp(toggle) {
-    let h = document.getElementById('help-center')
-    let visible = isInViewport(h)
-    let f = (toggle) ? 'fade-in' : 'fade-out'
-    console.log({f, visible})
-    if (visible) {
-        h.setAttribute('class', `center ${f}`)
-    } else {
-        h.setAttribute('class', `fade-out`)
-        let hinline = document.getElementById('help-inline')
-        if (hinline) {
-            hinline.setAttribute('class', f)
-        } else createInlineHelper(f)
-    }
-    //  ? 'center' : 'inline'
-    // let f = (toggle) ? 'fade-in' : 'fade-out'
-    // console.log("if help visible: " + isInViewport(h))
-    // let c = `${d} ${f}`
-    // h.setAttribute('class', c)
-}
-
-function toggleHelpv1(toggle) {
-    let h = document.getElementById('help')
-    let d = (isInViewport(h)) ? 'center' : 'inline'
-    let f = (toggle) ? 'fade-in' : 'fade-out'
-    console.log("if help visible: " + isInViewport(h))
-    let c = `${d} ${f}`
-    h.setAttribute('class', c)
-}
-
-function createInlineHelper(fade) {
-    let s = document.createElement('span')
-    s.setAttribute('id', `help-inline`)
-    s.setAttribute('class', `inline ${fade}`)
-    s.innerText = 'type "help" then Enter to get started'
-    // document.getElementById('console').appendChild(s)
-    // insertAfter(s, document.getElementById('console-cursor'))
-    document.getElementById('console-cursor').after(s)
-}
-
-function addEventCommand() {
-    let i = document.getElementById('console-input-current')
-    i.addEventListener('input', function (e) {
-        let le = Number(i.value.length)
-        const ts = 8.8 // refactor
-        let wi = le * ts
-        i.setAttribute('style', `width: ${wi}px`)
-
-        let s = (wi > 1) ? false : true
-        
-        // let c = (wi > 1) ? 'fade-out' : 'fade-in'
-        // let h = document.getElementById('help')
-        // let isinvp = isInViewport(h)
-        // console.log({isinvp})
-        // h.setAttribute('class', c)
-
-        toggleHelp(s)
-
-    })
-    i.addEventListener('keyup', function (e) {
-        const key = e.key
-        if (key === 'Enter') {
-            let v = e.target.value
-
-            i.removeAttribute('id')
-
-            let cc = document.getElementById('console-cursor')
-            cc.remove()
-
-            let l = document.getElementById('console-label-current')
-            let clv = l.innerHTML
-            l.removeAttribute('id')
-
-            let h = document.getElementById('help-inline')
-            if (h) h.remove()
-
-            i.setAttribute('readonly', 'true')
-            if (v) {
-                invokeCommand(v, l.value)
-            }
-            else {
-                let c = document.getElementById('console')
-                c.appendChild(document.createElement('br'))
-                createInput(clv)
-            }
-        }
-
-        if (key === 'ArrowUp' || key === 'ArrowDown') {
-            getCommandList(key)
-        }
-    })
-    window.onclick = () => {
-        i.focus()
-    }
-
+    createInput(path)
 }
 
 function createInput(path) {
@@ -349,6 +39,146 @@ function createInput(path) {
 
     nci.focus()
     addEventCommand()
+    createInlineHelper(true)
+}
+
+function printConsoleResult(results, type = 'success') {
+    // TO-DO - type each character
+    // Create result span tag
+    let c = document.getElementById('console')
+    let cr = document.createElement('div')
+    cr.setAttribute('class', `console-result console-result-${type}`)
+    cr.innerHTML = results
+    c.appendChild(cr)
+
+    c.appendChild(document.createElement('br'))
+}
+
+// Events
+function addEventCommand() {
+    let i = document.getElementById('console-input-current')
+    i.addEventListener('input', function (e) {
+        let le = Number(i.value.length)
+        let wi = le * _CHAR_SIZE
+        i.setAttribute('style', `width: ${wi}px`)
+
+        let s = (wi > 1) ? false : true
+
+        toggleHelp(s)
+
+    })
+    i.addEventListener('keyup', function (e) {
+        const key = e.key
+        if (key === 'Enter') {
+
+            document.getElementById('console-cursor').remove()
+            document.getElementById('help-inline').remove()
+
+            i.removeAttribute('id')
+            i.setAttribute('readonly', 'true')
+
+            let l = document.getElementById('console-label-current')
+            l.removeAttribute('id')
+
+            let v = e.target.value
+            if (v) {
+                invokeCommand(v, l.value)
+            }
+            else {
+                let c = document.getElementById('console')
+                c.appendChild(document.createElement('br'))
+                createInput(l.innerHTML)
+            }
+        }
+
+        if (key === 'ArrowUp' || key === 'ArrowDown') {
+            getCommandList(key)
+        }
+    })
+    window.onclick = () => {
+        i.focus()
+    }
+}
+
+// Commands
+function getCommandList(key) {
+    const max = _commandList.length
+    let i = document.getElementById('console-input-current')
+    const v = i.value
+    if (_cmd_idx === null) _cmd_idx = max
+
+    if (key === 'ArrowUp' && _cmd_idx != 0) {
+        _cmd_idx = _cmd_idx - 1
+    } 
+
+    if (key === 'ArrowDown' && _cmd_idx < (max - 1)) {
+        _cmd_idx = _cmd_idx + 1
+    }
+
+    const c = _commandList[_cmd_idx]
+    const le = (c) ? c.length : null
+
+    if (c && (c != v)) {
+        const ts = 8.8
+        const wi = le * ts
+        i.setAttribute('style', `width: ${wi}px`)
+        i.value = c
+    } else {
+        i.focus()
+        i.setSelectionRange(le, le * 10)
+    }
+}
+
+function addToCommandList(command) {
+    const last = _commandList.at(-1)
+    if (last != command) {
+        _commandList.push(command)
+        _cmd_idx = _commandList.length
+    }
+}
+
+async function invokeCommand(cmd, path) {
+    let err = `'${cmd}' is not recognized as an internal or external command,<br>operable program or batch file.`
+    cmd = cmd.toLowerCase()
+
+    if (cmd) {
+        const c = cmd.split(' ')
+        const cmdName = c[0]
+        const cmdAttributes = c[1]
+
+        // {Check} if text file exist
+        let text = await loadTextFile(`${cmdName}.txt`)
+        if (text) {
+            err = null
+            printConsoleResult(text)
+        }
+
+        // Check if json file exist
+        let json = await loadJsonFile(`${cmdName}.json`)
+        if (json && cmdAttributes && cmdAttributes.length > 0) {
+            let res = json[cmdAttributes] || null
+            if (res) {
+                err = null
+                printConsoleResult(prettyPrintChild(res))
+            }
+        }
+        if (json && !cmdAttributes) {
+            let res = json
+            err = null
+            printConsoleResult(`<div class="${cmdName}">${prettyPrint(res)}</div>`)
+        }
+    }
+
+    if (cmd == 'cls') {
+        err = ''
+        let c = document.getElementById('console')
+        removeAllChildNodes(c)
+    }
+    
+    if (err) printConsoleResult(err, 'error')
+    if (!err) addToCommandList(cmd)
+    createInput(_userPath)
+    toggleHelp(true)
 }
 
 function removeAllChildNodes(parent) {
@@ -357,22 +187,121 @@ function removeAllChildNodes(parent) {
     }
 }
 
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    let left = rect.left
-    let top = rect.top
-    let bottom = rect.bottom
-    let right = rect.right
-    console.log({left, top, right, bottom})
-    let win_height = window.innerHeight
-    let win_width = window.innerWidth
-    let cli_height = document.documentElement.clientHeight
-    let cli_width = document.documentElement.clientWidth
-    console.log({win_height, win_width, cli_height, cli_width})
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+// Load files
+async function loadCV(filePath) {
+    let res = await fetch(filePath)
+    _cv = await res.json()
+}
+
+async function loadJsonFile(fileName) {
+    try {
+        let res = await fetch(fileName)
+        if (res.ok) return res.json()
+    } catch(err) {
+        // do nothing
+    }
+}
+
+async function loadTextFile(fileName) {
+    try {
+        let res = await fetch(fileName)
+        if (res.ok) return res.text()
+    } catch(err) {
+        // do nothing
+    }
+}
+
+
+// Help
+function toggleHelp(toggle) {
+    let f = (toggle) ? 'fade-in' : 'fade-out'
+    let h = document.getElementById('help-inline')
+    h.setAttribute('class', `inline ${f}`)
+}
+
+function createInlineHelper(fade) {
+    let s = document.createElement('span')
+    s.setAttribute('id', `help-inline`)
+    s.setAttribute('class', `inline ${fade}`)
+    s.innerText = 'type "help" then Enter to get started'
+    document.getElementById('console-cursor').after(s)
+}
+
+// Pretty print
+function prettyPrint(value, key = null, pad = 0) {
+    const cn = value.constructor.name
+    let pretty
+    if (cn === 'Object') {
+        let na = []
+        for (const [k, v] of Object.entries(value)) {
+            let pkey = k.toUpperCase().replaceAll('_', ' ')
+            let p =  `<div class="Parent-Object"><div class="key parent">${pkey}</div>${prettyPrintChild(v, k, k, 0)}</div>`
+            na.push(p)
+        }
+        pretty = `<div class="Object">${na.join('')}</div>`
+    }
+
+    return pretty
+}
+
+function prettyPrintChild(value, key = null, parent = null, pad = 0) {
+    const cn = value.constructor.name
+    let pretty
+    if (cn === 'String' || cn === 'Number') {
+        value = value.toString()
+        let r = prettyPrintString(value, key, parent, pad)
+        pretty = (key) ? `<div class="key-text-pair">${r}</div>` : r
+
+    }
+    if (cn === 'Array') {
+        let na = []
+        value.forEach((v, i, s) => {
+            const vcn = v.constructor.name
+            let e
+            if (vcn === 'String' || 'Number') {
+                let vStr = v.toString()
+                e = `<div class="element element-text">${prettyPrintChild(vStr, null, null, 0)}</div>`
+            }
+            if (vcn === 'Object') {
+                e = `<div class="element element-object">${prettyPrintChild(v, null, null, 0)}</div>`
+            }
+            if (vcn === 'Array') {e = ''} // TO-DO maybe
+            na.push(e)
+        })
+        let v = `<div class="array">${na.join('')}</div>`
+        pretty = (key && !(parent === key)) ? `<div class="key child">${capitalize(key)}</div>${v}` : v
+    }
+    if (cn === 'Object') {
+        let na = []
+        const kl = Object.keys(value).map(k => k.length)
+        const mx = Math.max(...kl)
+        for (const [k, v] of Object.entries(value)) {
+            let p = prettyPrintChild(v, k, key, mx)
+            na.push(p)
+        }
+        let r = na.join('')
+        pretty = `<div class="object">${r}</div>`
+    }
+
+    return pretty
+}
+
+function prettyPrintString(value, key = null, parent = null, pad = 0) {
+    let res
+    if (key) {
+        let k = capitalize(key).padEnd(pad, ' ')
+        let sv =  `<span class="value">${value}</span>`
+        res = (parent === key) 
+            ? `<span class="key hide" pad="${pad}">${k}</span>${sv}` 
+            : `<span class="key" pad="${pad}">${k}</span>${sv}`
+    } else {
+        res = `<span class="value">${value}</span>`
+    }
+
+    return res
+}
+
+// String function
+function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1).toLowerCase().replaceAll('_', ' ')
 }
